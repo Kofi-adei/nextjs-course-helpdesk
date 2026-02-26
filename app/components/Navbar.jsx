8327c8f1800b9a1b7c26909635cee8b97a11532f
Kofi <kofiadei18@gmail.com>
2026-02-26 17:24:56 +0000

    Add font, styles and logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Logo from './dojo-logo.png';
 
 //Component imports
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <>
             <nav>
+                <Image
+                    src={Logo}
+                    alt='helpdesk logo'
+                    width={70}
+                    quality={100}
+                    placeholder='blur'
+                />
                 <h1>Kofi Helpdesk</h1>
                 <Link href='/'>Dashboard</Link>
                 <Link href='/tickets'>Tickets</Link>
